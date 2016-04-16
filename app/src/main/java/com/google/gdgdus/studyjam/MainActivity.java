bd3e9c80ab919b492072d788037119ab37db2286
Pascal Welsch <pascal.welsch@gmail.com>
2016-04-16 16:48:05 +0200

    Easy gallery

--- app/src/main/java/com/google/gdgdus/studyjam/MainActivity.java
+++ app/src/main/java/com/google/gdgdus/studyjam/MainActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     int mCounter = 0;
+
+    private ImageView mImage;
+
+    private static List<Integer> mImages = Arrays.asList(
+            R.drawable.holiday,
+            R.drawable.cat1,
+            R.drawable.cat2
+    );
 
     private TextView mOutput;
 
@@ -17,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mOutput = (TextView) findViewById(R.id.output);
+        mImage = (ImageView) findViewById(R.id.image);
 
         Button button = (Button) findViewById(R.id.button);
         //noinspection ConstantConditions
@@ -25,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(final View v) {
-                mCounter++;
-                updateCounterUi();
+                nextImage();
             }
         });
 
@@ -37,13 +51,23 @@ public class MainActivity extends AppCompatActivity {
         decrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                mCounter--;
-                updateCounterUi();
+                previousImage();
             }
         });
     }
 
-    private void updateCounterUi() {
-        mOutput.setText("Counter: " + mCounter);
+    private void nextImage() {
+        mCounter++;
+        mCounter %= mImages.size();
+        mOutput.setText("Image #" + mCounter);
+        mImage.setImageDrawable(getResources().getDrawable(mImages.get(mCounter)));
+    }
+
+    private void previousImage() {
+        mCounter--;
+        mCounter += mImages.size();
+        mCounter %= mImages.size();
+        mOutput.setText("Image #" + mCounter);
+        mImage.setImageDrawable(getResources().getDrawable(mImages.get(mCounter)));
     }
 }
